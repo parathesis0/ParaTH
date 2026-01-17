cab8378b218c79fac7918d558b0ae9b44c3e512d
Para <parathesis5@gmail.com>
2026-01-17 21:25:20 +0800

    init

---
+++ ParaTH/src/Engine.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace ParaTH;
+
+public sealed class Engine : Game
+{
+    public Engine()
+    {
+        var gdm = new GraphicsDeviceManager(this);
+        
+        gdm.PreferredBackBufferWidth = 1280;
+        gdm.PreferredBackBufferHeight = 720;
+        gdm.IsFullScreen = false;
+        gdm.SynchronizeWithVerticalRetrace = true;
+    }
+    
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+        base.LoadContent();
+    }
+
+    protected override void UnloadContent()
+    {
+        base.UnloadContent();
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+        base.Draw(gameTime);
+    }
+}
---
+++ ParaTH/src/Program.cs
@@ -0,0 +1,9 @@
+﻿static class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        using var g = new ParaTH.Engine();
+        g.Run();
+    }
+}
