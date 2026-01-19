c31b5481eebaeabc717d61c892b579dcb9b36e87
Para <parathesis5@gmail.com>
2026-01-20 00:14:00 +0800

    learning rendering basics

--- ParaTH/src/Engine.cs
+++ ParaTH/src/Engine.cs
@@ -1,17 +1,30 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace ParaTH;
 
 public sealed class Engine : Game
 {
+    private DynamicVertexBuffer vertexBuffer = null!;
+    private IndexBuffer indexBuffer = null!;
+    
+    private VertexPositionColor[] vertexInfo = null!;
+    private short[] indexData = null!;
+    
+    private BasicEffect effect = null!;
+    
     public Engine()
     {
         var gdm = new GraphicsDeviceManager(this);
+        gdm.PreferredBackBufferWidth = 640;
+        gdm.PreferredBackBufferHeight = 480;
+        gdm.SynchronizeWithVerticalRetrace = false;
+        Content.RootDirectory = "Content";
         
-        gdm.PreferredBackBufferWidth = 1280;
-        gdm.PreferredBackBufferHeight = 720;
-        gdm.IsFullScreen = false;
-        gdm.SynchronizeWithVerticalRetrace = true;
+        IsFixedTimeStep = true;
+        TargetElapsedTime = TimeSpan.FromTicks(166667);
+        IsMouseVisible = true;
     }
     
     protected override void Initialize()
@@ -21,22 +34,72 @@ public sealed class Engine : Game
 
     protected override void LoadContent()
     {
+        vertexBuffer = new DynamicVertexBuffer(GraphicsDevice, typeof(VertexPositionColor), 3, BufferUsage.WriteOnly);
+        indexBuffer = new IndexBuffer(GraphicsDevice, IndexElementSize.SixteenBits, 3, BufferUsage.WriteOnly);
+
+        const float Size = 100;
+        
+        vertexInfo = 
+        [
+            new VertexPositionColor(new Vector3(    0, -Size, 0), Color.Red),
+            new VertexPositionColor(new Vector3( Size,  Size, 0), Color.Green),
+            new VertexPositionColor(new Vector3(-Size,  Size, 0), Color.Blue)
+        ];
+        var center = new Vector3(640f / 2, 480f / 2, 0);
+        foreach (ref var vpc in vertexInfo.AsSpan())
+            vpc.Position += center;
+        
+        indexData = [ 0, 1, 2 ];
+        
+        vertexBuffer.SetData(vertexInfo);
+        indexBuffer.SetData(indexData);
+
+        effect = new BasicEffect(GraphicsDevice);
+        effect.VertexColorEnabled = true;
+        effect.Projection = Matrix.CreateOrthographicOffCenter(0, 640, 480, 0, -1, 0);
+        
         base.LoadContent();
     }
 
     protected override void UnloadContent()
     {
+        vertexBuffer.Dispose();
+        indexBuffer.Dispose();
+        effect.Dispose();
         base.UnloadContent();
     }
 
     protected override void Update(GameTime gameTime)
     {
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape)) Exit();
+        
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+        GraphicsDevice.Clear(Color.Black);
+        GraphicsDevice.BlendState = BlendState.AlphaBlend;
+        GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+        GraphicsDevice.RasterizerState = RasterizerState.CullCounterClockwise;
+        GraphicsDevice.DepthStencilState = DepthStencilState.None;
+        
+        GraphicsDevice.SetVertexBuffer(vertexBuffer);
+        GraphicsDevice.Indices = indexBuffer;
+
+        foreach (var pass in effect.CurrentTechnique.Passes)
+        {
+            pass.Apply();
+            
+            GraphicsDevice.DrawIndexedPrimitives(
+                PrimitiveType.TriangleList,
+                0,
+                0,
+                3,
+                0,
+                1
+            );
+        }
         base.Draw(gameTime);
     }
 }
---
+++ ParaTH/src/SuperBatch.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ParaTH;
+
+public sealed class SuperBatch : IDisposable 
+{
+    public GraphicsDevice GraphicsDevice { get; private set; }
+    public bool IsDisposed { get; private set; }
+    
+    private DynamicVertexBuffer vertexBuffer = null!;
+    private IndexBuffer indexBuffer = null!;
+    
+    private VertexPositionColor[] vertexInfo = null!;
+    private short[] indexData = null!;
+    
+    private BasicEffect effect = null!;
+
+    public SuperBatch(GraphicsDevice graphicsDevice)
+    {
+        GraphicsDevice = graphicsDevice;
+    }
+    
+    ~SuperBatch()
+    {
+        Dispose(false);
+    }
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+    
+    private void Dispose(bool disposing)
+    {
+        if (IsDisposed) return;
+        
+        if (disposing)
+        {
+            vertexBuffer.Dispose();
+            effect.Dispose();
+        }
+
+        IsDisposed = true;
+    }
+}
